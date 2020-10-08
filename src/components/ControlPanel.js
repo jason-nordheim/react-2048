0c3d893425fabd738f7b72f5c762a468dec000e6
jason-nordheim <jason.nordheim@gmail.com>
2020-10-08 11:06:44 -0600

    created keyboard listening events for each directional arrow to move the tiles on the board on keyup

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,24 +1,39 @@
 import React from 'react'
+import useEventListener from '../hooks/useEventListener'
 
 function ControlPanel({ gameOver, started, startGame, slide }) {
+
+  // leverage event listener hook to
+  // listen for key presses
+  useEventListener("keyup", (event) => {
+    // do nothing if the game hasn't started
+    // do nothing if the event object is null
+    if (started && event) {
+      if (event.key === "ArrowRight") return slide("right");
+      else if (event.key === "ArrowLeft") return slide("left");
+      else if (event.key === "ArrowUp") return slide("up");
+      else if (event.key === "ArrowDown") return slide("down");
+    } 
+  });
+
   return (
-      <div className="controlPanel">
-        <button className="start_button" onClick={() => startGame()}>
-          {started ? "Restart" : "Start"}
-        </button>
-        <button disabled={gameOver} onClick={() => slide("up")}>
-          Up
-        </button>
-        <button disabled={gameOver} onClick={() => slide("down")}>
-          Down
-        </button>
-        <button disabled={gameOver} onClick={() => slide("left")}>
-          Left
-        </button>
-        <button disabled={gameOver} onClick={() => slide("right")}>
-          Right
-        </button>
-      </div>
+    <div className="controlPanel">
+      <button className="start_button" onClick={() => startGame()}>
+        {started ? "Restart" : "Start"}
+      </button>
+      <button disabled={gameOver} onClick={() => slide("up")}>
+        Up
+      </button>
+      <button disabled={gameOver} onClick={() => slide("down")}>
+        Down
+      </button>
+      <button disabled={gameOver} onClick={() => slide("left")}>
+        Left
+      </button>
+      <button disabled={gameOver} onClick={() => slide("right")}>
+        Right
+      </button>
+    </div>
   );
 }
 
